cc5605f723c7f6d71e7376ac205de10e60700ac9
Fang Ling <61139150+fang-ling@users.noreply.github.com>
2024-05-17 21:45:39 +0800

    Finished RT1 of Assignment 1

--- Memorize/Memorize/ContentView.swift
+++ Memorize/Memorize/ContentView.swift
@@ -8,31 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+  let emojis = [
+    "👻", "🎃", "🕷️", "😈", "💀", "🕸️", "🧙", "🙀", "🦁", "😱", "☠️", "🍭"
+  ]
+  
+  @State var card_count = 4
+  
   var body: some View {
-    HStack{
-      CardView(is_face_up: true)
-      CardView()
-      CardView()
-      CardView()
+    VStack {
+      ScrollView {
+        cards
+      }
+      Spacer()
+      card_count_adjusters
+    }
+    .padding()
+  }
+  
+  var cards: some View {
+    LazyVGrid(columns: [GridItem(.adaptive(minimum: 120))]) {
+      ForEach(0 ..< card_count, id: \.self) { i in
+        CardView(content: emojis[i])
+          .aspectRatio(2/3, contentMode: .fit)
+      }
     }
     .foregroundColor(.orange)
-    .padding()
+  }
+  
+  func card_count_adjuster(by offset: Int, symbol: String) -> some View {
+    Button(action: {
+      card_count += offset
+    }, label: {
+      Image(systemName: symbol)
+    })
+    .disabled(card_count + offset < 1 || card_count + offset > emojis.count)
+  }
+  
+  var card_remover: some View {
+    card_count_adjuster(by: -1, symbol: "rectangle.stack.badge.minus.fill")
+  }
+  
+  var card_adder: some View {
+    card_count_adjuster(by: +1, symbol: "rectangle.stack.badge.plus.fill")
+  }
+  
+  var card_count_adjusters: some View {
+    HStack {
+      card_remover
+      Spacer()
+      card_adder
+    }
+    .imageScale(.large)
+    .font(.largeTitle)
   }
 }
 
 struct CardView: View {
-  @State var is_face_up = false
+  let content: String
+  @State var is_face_up = true
   
   var body: some View {
     ZStack {
       let base = RoundedRectangle(cornerRadius: 12)
-      if is_face_up {
+      Group {
         base.fill(.white)
         base.strokeBorder(lineWidth: 2)
-        Text("👻").font(.largeTitle)
-      } else {
-        base.fill()
+        Text(content).font(.largeTitle)
       }
+      .opacity(is_face_up ? 1 : 0)
+      base.fill().opacity(is_face_up ? 0 : 1)
     }
     .onTapGesture {
       is_face_up.toggle()
